a4a1bd8b1c4f7ab237e9eb91d5165b3f82fb73b8
Angela Zhu <anz047@ucsd.edu>
2019-02-14 11:02:26 -0800

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,14 +17,31 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
-function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+ function addProjectDetails(e) {
+ 	// Prevent following the link
+ 	e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+ 	// Get the div ID, e.g., "project3"
+ 	var projectID = $(this).closest('.project').attr('id');
+ 	// get rid of 'project' from the front of the id 'project3'
+ 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+ 	console.log("User clicked on project " + idNumber);
+ 	$.get("/"+idNumber, callBackFunc);
+ }
+
+// function addProject(result) {
+//   var projectHTML = '<a href="#" class="thumbnail">' +
+//     '<img src="' + result['image'] + '" class="img">' +
+//     '<p>' + result['title'] + '</p>' +
+//     '<p><small>' + result['date'] +
+//     '</small></p></a>'; 
+// }
+
+function callBackFunc(result){
+	console.log(result.image);
+	console.log("#"+result.id+" "+".details");
+	$("#project"+result.id+" "+".details").append("<img class=detailsImage src="+result.image+" />");
+	$("#project"+result.id+" "+".details").append("<h5>"+result.title+"</h5>");
+	$("#project"+result.id+" "+".details").append(result.summary);
 }
